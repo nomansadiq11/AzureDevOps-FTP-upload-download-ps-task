f79b4fc60fa4c19c0b2c53ffef0806f608d7aecb
nomansadiq11 <nomi.cs786@gmail.com>
2019-09-24 21:22:39 +0400

    ftp single file upload code

---
+++ task.ps1
@@ -0,0 +1,18 @@
+﻿
+
+# create the FtpWebRequest and configure it
+$ftp = [System.Net.FtpWebRequest]::Create("ftp://nomansadiq.pk:21/fromftp.zip")
+$ftp = [System.Net.FtpWebRequest]$ftp
+$ftp.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+$ftp.Credentials = new-object System.Net.NetworkCredential("tiles","dohbxlZqpwta:gm5cuvn")
+$ftp.UseBinary = $true
+$ftp.UsePassive = $true
+# read in the file to upload as a byte array
+$content = [System.IO.File]::ReadAllBytes("G:\Documents\Website\Tiles\fromftp.zip")
+$ftp.ContentLength = $content.Length
+# get the request stream, and write the bytes into it
+$rs = $ftp.GetRequestStream()
+$rs.Write($content, 0, $content.Length)
+# be sure to clean up after ourselves
+$rs.Close()
+$rs.Dispose()
