d043bf6eacbe5f3d9bfef7c47a1f491fffdae5b4
Noman Sadiq <nomi.cs786@gmail.com>
2019-09-30 17:31:18 +0400

    updated code as uploading files

---
+++ workingfile.ps1
@@ -0,0 +1,39 @@
+
+$ftp_uri = "ftp://nomansadiq.pk/"
+$user = "tiles"
+$pass = "dohbxlZqpwta:gm5cuvn"
+$path= "C:\Data\Temp\fromftp\"
+$webclient = New-Object System.Net.WebClient
+$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+
+
+
+
+
+ foreach($item in Get-ChildItem -recurse $path)
+{
+  $relpath =    [system.io.path]::GetFullPath($item.FullName).SubString([system.io.path]::GetFullPath($path).Length )
+  Write-Host $relpath
+        if ($item.Attributes -eq "Directory")
+         {
+            try
+             {
+                Write-Host Creating $item.Name
+                $makeDirectory = [System.Net.WebRequest]::Create($ftp_uri+$relpath);
+                $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+                $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
+                $makeDirectory.GetResponse();
+            }
+            catch [Net.WebException]
+             {
+                Write-Host $item.Name Directory may be already exists.
+            }
+            continue;
+        }
+        "Uploading $item to :  $relpath"
+
+        "ftp url :  $ftp_uri + $relpath"
+
+        $uri = New-Object System.Uri($ftp_uri+$relpath)
+        $webclient.UploadFile($uri, $item.FullName)
+}
