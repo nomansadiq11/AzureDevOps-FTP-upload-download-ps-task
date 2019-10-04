92dc7f75b8a174017438fde3684c1ebf9d250744
nomansadiq11 <nomi.cs786@gmail.com>
2019-10-04 19:05:19 +0400

    inditation fix and added some logs

--- vsts-file.ps1
+++ vsts-file.ps1
@@ -7,34 +7,50 @@ $pass = "dohbxlZqpwta:gm5cuvn"
 $path= $env:hellopakistan
 
 
-$webclient = New-Object System.Net.WebClient
-$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+
+
 
  foreach($item in Get-ChildItem -recurse $path)
 {
-  $relpath =    [system.io.path]::GetFullPath($item.FullName).SubString([system.io.path]::GetFullPath($path).Length + 1 )
-  Write-Host $relpath
-        if ($item.Attributes -eq "Directory")
-         {
-            try
-             {
-                Write-Host Creating $item.Name
-                $makeDirectory = [System.Net.WebRequest]::Create($ftp_uri+$relpath);
-                $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
-                $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
-                # $makeDirectory.UsePassive = $true;
-                $makeDirectory.GetResponse();
-            }
-            catch [Net.WebException]
-             {
-                Write-Host $item.Name Directory may be already exists.
-            }
-            continue;
+    
+
+    $relpath =    [system.io.path]::GetFullPath($item.FullName).SubString([system.io.path]::GetFullPath($path).Length + 1 )
+
+    Write-Host $relpath
+
+    if ($item.Attributes -eq "Directory")
+    {
+        try
+        {
+            Write-Host Creating $item.Name
+            $makeDirectory = [System.Net.WebRequest]::Create($ftp_uri+$relpath);
+            $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+            $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
+            # $makeDirectory.UsePassive = $true;
+            $makeDirectory.UseBinary = $true
+            $makeDirectory.UsePassive = $true
+            $makeDirectory.GetResponse();
         }
-        "Uploading $item to :  $relpath"
+        catch [Net.WebException]
+        {
+            Write-Host $item.Name Directory may be already exists.
+        }
+        continue;
+    }
+    "Uploading $item to :  $relpath"
 
-        "ftp url :  $ftp_uri + $relpath"
+    "ftp url :  $ftp_uri + $relpath"
 
-        $uri = New-Object System.Uri($ftp_uri+$relpath)
-        $webclient.UploadFile($uri, $item.FullName)
+
+    $webclient = New-Object System.Net.WebClient
+    Write-Host "created new object"
+    $webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+    Write-Host "added credentials"
+    $uri = New-Object System.Uri($ftp_uri+$relpath)
+    Write-Host "new url"
+    $webclient.UploadFile($uri, $item.FullName)
+    Write-Host "upl0ad done file "
+
+    
+    
 }
