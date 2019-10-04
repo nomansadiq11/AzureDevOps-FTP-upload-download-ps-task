bf783309fe6e69019d58120ab29cbd6cf2809c81
nomansadiq11 <nomi.cs786@gmail.com>
2019-10-04 16:08:52 +0400

    code compatible with vsts task

---
+++ vsts-file.ps1
@@ -0,0 +1,40 @@
+
+$ftp_uri = "ftp://nomansadiq.pk/"
+$user = "tiles"
+$pass = "dohbxlZqpwta:gm5cuvn"
+
+
+$path= $env:hellopakistan
+
+
+$webclient = New-Object System.Net.WebClient
+$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+
+ foreach($item in Get-ChildItem -recurse $path)
+{
+  $relpath =    [system.io.path]::GetFullPath($item.FullName).SubString([system.io.path]::GetFullPath($path).Length + 1 )
+  Write-Host $relpath
+        if ($item.Attributes -eq "Directory")
+         {
+            try
+             {
+                Write-Host Creating $item.Name
+                $makeDirectory = [System.Net.WebRequest]::Create($ftp_uri+$relpath);
+                $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+                $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
+                # $makeDirectory.UsePassive = $true;
+                $makeDirectory.GetResponse();
+            }
+            catch [Net.WebException]
+             {
+                Write-Host $item.Name Directory may be already exists.
+            }
+            continue;
+        }
+        "Uploading $item to :  $relpath"
+
+        "ftp url :  $ftp_uri + $relpath"
+
+        $uri = New-Object System.Uri($ftp_uri+$relpath)
+        $webclient.UploadFile($uri, $item.FullName)
+}
