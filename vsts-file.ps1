a32d18c6f89b616187262501858372546f91e1cd
nomansadiq11 <nomi.cs786@gmail.com>
2019-10-08 22:40:45 +0400

    updated for uploading the file but still fail

--- vsts-file.ps1
+++ vsts-file.ps1
@@ -38,20 +38,44 @@ $path= $env:hellopakistan
     "ftp url :  $ftp_uri + $relpath"
 
 
-    $webclient = New-Object System.Net.WebClient
-    Write-Host "created new object"
-    $webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
-    Write-Host "added credentials"
-    $uri = New-Object System.Uri($ftp_uri+$relpath)
-    Write-Host "new url"
-    $webclient.UploadFile($uri, $item.FullName)
-    Write-Host "upl0ad done file "
-    $webclient.Dispose()
-    Write-Host "close connection"
+    try {
 
-    Write-Host "2 sec wait start"
-    Start-Sleep -Seconds 2
-    Write-Host "2 sec wait end"
+        $uri = New-Object System.Uri($ftp_uri+$relpath)
+        # $webclient.UploadFile($uri, $item.FullName)
+
+        $makefile = [System.Net.WebRequest]::UploadFile($uri, $item.FullName);
+        $makefile.Credentials = New-Object System.Net.NetworkCredential($user,$pass);
+        $makefile.Method = [System.Net.WebRequestMethods+FTP]::UploadFile;
+        $makefile.UsePassive = $false; 
+        $makefile.GetResponse();
+        $makefile.Dispose(); 
+        
+    }
+    catch  {
+        Write-Host $item.Name "error creating" $_.Exception.Message
+        break;
+    }
+
+    
+
+    
+
+
+    # $webclient = New-Object System.Net.WebClient
+    # Write-Host "created new object"
+    # $webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
+    # Write-Host "added credentials"
+    # $uri = New-Object System.Uri($ftp_uri+$relpath)
+    # Write-Host "new url"
+    # $webclient.UsePassive = $false
+    # $webclient.UploadFile($uri, $item.FullName)
+    # Write-Host "upl0ad done file "
+    # $webclient.Dispose()
+    # Write-Host "close connection"
+
+    # Write-Host "2 sec wait start"
+    # Start-Sleep -Seconds 2
+    # Write-Host "2 sec wait end"
 
 
     
